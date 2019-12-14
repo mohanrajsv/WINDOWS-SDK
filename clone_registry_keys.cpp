9c64e8cedf6dfc1a085c1c60e83d227bc9b76bae
Mohanraj S <47392201+mohanrajsv@users.noreply.github.com>
2019-12-14 23:41:53 +0530

    Update clone_registry_keys.cpp

--- clone_registry_keys.cpp
+++ clone_registry_keys.cpp
@@ -1,103 +1,129 @@
 #include <windows.h>
 #include <tchar.h>
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 #define MAX_KEY_LENGTH 255
 #define MAX_VALUE_NAME 20000
 
-void QueryKey(HKEY hKey,HKEY hDestKey) 
+void QueryKey(HKEY hSrcKey, HKEY hDestKey)
 {
+    /*
+    LSTATUS RegQueryInfoKeyW(
+        HKEY      hKey,
+        LPWSTR    lpClass,
+        LPDWORD   lpcchClass,
+        LPDWORD   lpReserved,
+        LPDWORD   lpcSubKeys,
+        LPDWORD   lpcbMaxSubKeyLen,
+        LPDWORD   lpcbMaxClassLen,
+        LPDWORD   lpcValues,
+        LPDWORD   lpcbMaxValueNameLen,
+        LPDWORD   lpcbMaxValueLen,
+        LPDWORD   lpcbSecurityDescriptor,
+        PFILETIME lpftLastWriteTime
+    );*/
+
     TCHAR achClass[MAX_PATH];                   // buffer for class name & max_path sizw is 260 here
     DWORD cchClassName = MAX_PATH;                // size of class string 
-    DWORD cSubKeys=0;                            // number of subkeys 
+    DWORD cSubKeys = 0;                            // number of subkeys 
     DWORD cbMaxSubKey;                           // longest subkey size 
     DWORD cchMaxClass;                           // longest class string 
     DWORD cValues;                               // number of values for key 
-    DWORD type;                                
+    DWORD type;
     DWORD cchMaxValue;                          // longest value name 
     DWORD cbMaxValueData;                       // longest value data 
     DWORD cbSecurityDescriptor;                 // size of security descriptor 
     FILETIME ftLastWriteTime;                    // last write time 
-    DWORD i, retCode,result; 
-    TCHAR achValue[MAX_VALUE_NAME]; 
-    DWORD cchValue = MAX_VALUE_NAME; 
-    cchValue++;
+    DWORD i, retCode, result;
+    DWORD cchValue = MAX_VALUE_NAME;
+    ///cchValue++;
     LPSTR lpName = NULL, lpData = NULL;
-    retCode = RegQueryInfoKey(
-        hKey,                    
-        achClass,               
-        &cchClassName,           
-        NULL,                    
-        &cSubKeys,               
-        &cbMaxSubKey,            
-        &cchMaxClass,            
-        &cValues,                
-        &cchMaxValue,            
-        &cbMaxValueData,         
-        &cbSecurityDescriptor,  
-        &ftLastWriteTime);     
- 	lpName = (LPSTR)malloc(50*cchMaxValue);
-    lpData = (LPSTR)malloc(50*cbMaxValueData);
-    if(retCode!=ERROR_SUCCESS)
-        return;
 
-    if (cValues) 
+    retCode = RegQueryInfoKeyW(
+        hSrcKey,
+        achClass,
+        &cchClassName,
+        NULL,
+        &cSubKeys,
+        &cbMaxSubKey,
+        &cchMaxClass,
+        &cValues,
+        &cchMaxValue,
+        &cbMaxValueData,
+        &cbSecurityDescriptor,
+        &ftLastWriteTime);
+    if (retCode != ERROR_SUCCESS)
     {
-        system("cls");
-        for (i=0;i<cValues; i++) 
+        return;
+    }
+    cbMaxSubKey++;
+    cchMaxValue++;
+
+    if (cchMaxValue > cbMaxSubKey)
+        cbMaxSubKey = cchMaxValue;
+    
+    lpName = (LPSTR)malloc(sizeof(DWORD)*cchMaxValue);
+    lpData = (LPSTR)malloc(sizeof(DWORD) * cbMaxValueData);
+    if (cValues)
+    {
+        for (int i = 0; i < cValues; i++)
         {
-            cchValue = cchMaxValue; 
-            DWORD dSize=cbMaxValueData;
-            retCode = RegEnumValue(hKey, i, lpName, &cchValue, NULL, &type, (LPBYTE)lpData, &dSize);
-            
-            RegSetValueEx(hDestKey, lpName, 0, type, (LPBYTE)lpData, dSize);
+            cchValue = cchMaxValue;
+            cchValue++;
+            DWORD dSize = cbMaxValueData;
+            dSize++;
+            retCode = RegEnumValueW(hSrcKey, i, (LPWSTR)lpName, &cchValue, NULL, &type, (LPBYTE)lpData, &dSize);
+            RegSetValueExW(hDestKey, (LPCWSTR)lpName, 0, type, (LPBYTE)lpData, dSize);
         }
     }
-	
-        for (DWORD idx=0;idx<cSubKeys; idx++) 
-        {
-            HKEY hKeySrc,hKeyDest;
-            DWORD namesz=cbMaxSubKey;
-            cchValue = MAX_VALUE_NAME;
-            RegEnumKeyEx(hKey, idx, achClass, &cchValue, NULL, NULL, NULL, NULL);
-            result = RegOpenKeyEx(hKey, achClass, 0L, KEY_READ, &hKeySrc);
+    if (lpData != NULL) {
+        //free(lpData);
+        lpData = NULL;
+    }
 
-		    if (result != ERROR_SUCCESS)
-			    return;
-            RegCreateKeyEx(hDestKey, achClass, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKeyDest, NULL);
 
-            QueryKey(hKeySrc,hKeyDest);                  //recursivly calling for subkey of subkey and so on... :(
+    for (DWORD idx = 0; idx < cSubKeys; idx++)
+    {
+        HKEY hKeySrc, hKeyDest;
+        DWORD namesz = cbMaxSubKey;
+        namesz++;
+        RegEnumKeyEx(hSrcKey, idx, (LPWSTR)lpName, &namesz, NULL, NULL, NULL, NULL);
+        result = RegOpenKeyExW(hSrcKey, (LPWSTR)lpName, 0L, KEY_READ, &hKeySrc);
 
-            RegCloseKey(hKeySrc);
-		    RegCloseKey(hKeyDest);
-        }
-	lpName = NULL;
+        if (result != ERROR_SUCCESS)
+            return;
+        RegCreateKeyExW(hDestKey, (LPWSTR)lpName, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKeyDest, NULL);
+        
+        QueryKey(hKeySrc, hKeyDest);                  //recursivly calling for subkey of subkey and so on... :(
+
+        RegCloseKey(hKeySrc);
+        RegCloseKey(hKeyDest);
+    }
+    
+    lpName = NULL;
 }
+
 
 int main(void)
 {
     bool src = true, dest = true;
-    HKEY hTestKey,hDestKey;
-    if(RegOpenKeyEx( HKEY_LOCAL_MACHINE,TEXT("SOFTWARE\\Wow6432Node\\AdventNet\\DesktopCentral\\DCAgent"),0,KEY_READ,&hTestKey) != ERROR_SUCCESS)
+    HKEY hTestKey, hDestKey;
+    if (RegOpenKeyExW(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\Wow6432Node\\AdventNet\\DesktopCentral\\DCAgent"), 0, KEY_READ, &hTestKey) != ERROR_SUCCESS)
     {
-       cout<<"\nSOURCE KEY ERROR\n";
-       src=false;
+        src = false;
     }
-
-    if(RegOpenKeyEx( HKEY_LOCAL_MACHINE,TEXT("SOFTWARE\\Wow6432Node\\AdventNet\\DesktopCentral\\zohocopy1"),0,KEY_READ,&hDestKey) == ERROR_FILE_NOT_FOUND)
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("SOFTWARE\\Wow6432Node\\AdventNet\\DesktopCentral\\clone"), 0, KEY_READ, &hDestKey) == ERROR_FILE_NOT_FOUND)
     {
-       if (RegCreateKeyEx(HKEY_LOCAL_MACHINE, "SOFTWARE\\Wow6432Node\\AdventNet\\DesktopCentral\\zohocopy1", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hDestKey, NULL) != ERROR_SUCCESS)
-       dest=false;
+        if (RegCreateKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Wow6432Node\\AdventNet\\DesktopCentral\\clone", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &hDestKey, NULL) != ERROR_SUCCESS)
+            dest = false;
     }
-
-    if(src==false || dest==false)
-        cout<<"ERROR HAS BEEN OCCURED";
+    if (src == false || dest == false)
+        cout << "ERROR HAS BEEN OCCURED";
     else
     {
-        QueryKey(hTestKey,hDestKey);
-        cout<<"\n\t\tdone\n";
+        QueryKey(hTestKey, hDestKey);
+        cout << " \t\t Cloning Mission completed \n " <<endl ;
     }
-
     RegCloseKey(hTestKey);
     RegCloseKey(hDestKey);
     return 0;
